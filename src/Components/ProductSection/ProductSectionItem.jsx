bd98c6e60e2226b63e76077601a4beef1a17979a
Pavithran2 <pavithrank02@gmail.com>
2024-03-04 10:27:09 +0530

    wishlist completed

--- src/Components/ProductSection/ProductSectionItem.jsx
+++ src/Components/ProductSection/ProductSectionItem.jsx
@@ -26,6 +26,7 @@ const ProductSectionItem = ({
 }) => {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart.cart);
+  const wishList = useSelector((state) => state.wishList.wishList);
   // console.log(cart,)
 
   const defaultSize = size[0];
@@ -119,22 +120,40 @@ const ProductSectionItem = ({
               color="white"
               ripple={true}
             >
+              {wishList.length > 0 && wishList.filter((item) => item.id === id).length > 0 ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="black"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="#000"
+                  className="w-6 h-6"
 
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="#000"
-                className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="#000"
+                  className="w-6 h-6"
 
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                />
-              </svg>
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                  />
+                </svg>
+              )}
+
             </Button>
           </Tooltip>
 
